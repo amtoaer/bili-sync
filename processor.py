bbde9d6ba66e53cae44fe9afde86e7133dea9223
amtoaer <amtoaer@gmail.com>
2023-11-30 18:12:15 +0800

    fix: 修复判断

--- processor.py
+++ processor.py
@@ -19,7 +19,7 @@ from settings import settings
 
 client = httpx.AsyncClient(headers=HEADERS)
 
-anchor = datetime.datetime.today()
+anchor = datetime.date.today()
 
 
 async def cleanup() -> None:
@@ -95,10 +95,10 @@ async def manage_model(medias: list[dict], fav_list: FavoriteList) -> None:
 
 async def process() -> None:
     global anchor
-    if datetime.datetime.now() > anchor and await credential.check_refresh():
+    if datetime.date.today() >= anchor and await credential.check_refresh():
         try:
             await credential.refresh()
-            anchor = datetime.datetime.today() + datetime.timedelta(days=1)
+            anchor = datetime.date.today() + datetime.timedelta(days=1)
             logger.info("Credential refreshed.")
         except Exception:
             logger.exception("Failed to refresh credential.")
