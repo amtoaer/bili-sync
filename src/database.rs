79bbc9e4b60c6e52057fc9919c3c0e2e86e8bfd0
amtoaer <amtoaer@gmail.com>
2024-03-27 23:26:38 +0800

    refactor: 使用更简单的写法获取数据库连接

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,6 @@
-use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use sea_orm::{Database, DatabaseConnection};
 
 use crate::Result;
 pub async fn database_connection() -> Result<DatabaseConnection> {
-    let opt = ConnectOptions::new("sqlite://./data.sqlite?mode=rwc");
-    Ok(Database::connect(opt).await?)
+    Ok(Database::connect("sqlite://./data.sqlite?mode=rwc").await?)
 }
