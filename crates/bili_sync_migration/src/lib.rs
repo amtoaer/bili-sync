5177d86b752a094fbe34708436b4b44c452a035a
amtoaer <amtoaer@gmail.com>
2025-01-22 16:44:28 +0800

    feat: 为各个 video list 表添加 latest_row_at 字段

--- crates/bili_sync_migration/src/lib.rs
+++ crates/bili_sync_migration/src/lib.rs
@@ -4,6 +4,7 @@ mod m20240322_000001_create_table;
 mod m20240505_130850_add_collection;
 mod m20240709_130914_watch_later;
 mod m20240724_161008_submission;
+mod m20250122_062926_add_latest_row_at;
 
 pub struct Migrator;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20240505_130850_add_collection::Migration),
             Box::new(m20240709_130914_watch_later::Migration),
             Box::new(m20240724_161008_submission::Migration),
+            Box::new(m20250122_062926_add_latest_row_at::Migration),
         ]
     }
 }
