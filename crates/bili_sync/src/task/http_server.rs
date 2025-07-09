1d6a75fafdbcba40830900ef0eb2b4a6ca2edaca
amtoaer <amtoaer@gmail.com>
2025-07-09 23:51:27 +0800

    chore: 他又改回去了

--- crates/bili_sync/src/task/http_server.rs
+++ crates/bili_sync/src/task/http_server.rs
@@ -1,20 +1,22 @@
+use std::collections::HashSet;
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
 use axum::extract::Request;
-use axum::http::{Uri, header};
+use axum::http::header;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Extension, ServiceExt};
 use reqwest::StatusCode;
-use rust_embed::Embed;
+use rust_embed_for_web::{EmbedableFile, RustEmbed};
 use sea_orm::DatabaseConnection;
 
 use crate::api::{MpscWriter, router};
 use crate::bilibili::BiliClient;
 use crate::config::VersionedConfig;
 
-#[derive(Embed)]
+#[derive(RustEmbed)]
+#[preserve_source = false]
 #[folder = "../../web/build"]
 struct Asset;
 
@@ -36,14 +38,44 @@ pub async fn http_server(
     Ok(axum::serve(listener, ServiceExt::<Request>::into_make_service(app)).await?)
 }
 
-async fn frontend_files(uri: Uri) -> impl IntoResponse {
-    let mut path = uri.path().trim_start_matches('/');
+async fn frontend_files(request: Request) -> impl IntoResponse {
+    let mut path = request.uri().path().trim_start_matches('/');
     if path.is_empty() || Asset::get(path).is_none() {
         path = "index.html";
     }
     let Some(content) = Asset::get(path) else {
         return (StatusCode::NOT_FOUND, "404 Not Found").into_response();
     };
-    let mime = mime_guess::from_path(path).first_or_octet_stream();
-    ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+    let mime_type = content.mime_type();
+    let content_type = mime_type.as_deref().unwrap_or("application/octet-stream");
+    if cfg!(debug_assertions) {
+        (
+            [(header::CONTENT_TYPE, content_type)],
+            // safety: `RustEmbed` returns uncompressed files directly from the filesystem in debug mode
+            content.data().unwrap(),
+        )
+            .into_response()
+    } else {
+        let accepted_encodings = request
+            .headers()
+            .get(header::ACCEPT_ENCODING)
+            .and_then(|v| v.to_str().ok())
+            .map(|s| s.split(',').map(str::trim).collect::<HashSet<_>>())
+            .unwrap_or_default();
+        for (encoding, data) in [("br", content.data_br()), ("gzip", content.data_gzip())] {
+            if accepted_encodings.contains(encoding) {
+                if let Some(data) = data {
+                    return (
+                        [
+                            (header::CONTENT_TYPE, content_type),
+                            (header::CONTENT_ENCODING, encoding),
+                        ],
+                        data,
+                    )
+                        .into_response();
+                }
+            }
+        }
+        "Unsupported Encoding".into_response()
+    }
 }
