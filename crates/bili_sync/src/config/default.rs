2f912152839c5d35a716fd6f57114eb42bdb91b7
amtoaer <amtoaer@gmail.com>
2025-07-11 20:28:54 +0800

    chore: 更新 rust 依赖

--- crates/bili_sync/src/config/default.rs
+++ crates/bili_sync/src/config/default.rs
@@ -1,4 +1,4 @@
-use rand::seq::SliceRandom;
+use rand::seq::IndexedRandom;
 
 pub(super) fn default_time_format() -> String {
     "%Y-%m-%d".to_string()
@@ -7,7 +7,7 @@ pub(super) fn default_time_format() -> String {
 /// 默认的 auth_token 实现，生成随机 16 位字符串
 pub(super) fn default_auth_token() -> String {
     let byte_choices = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     (0..16)
         .map(|_| *(byte_choices.choose(&mut rng).expect("choose byte failed")) as char)
         .collect()
