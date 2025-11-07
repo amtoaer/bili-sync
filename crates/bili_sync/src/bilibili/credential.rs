a735422f2587a0eb7cf369af8af925e34ffef5bd
amtoaer <amtoaer@gmail.com>
2025-11-07 21:27:04 +0800

    feat: 视频源现在会乱序处理，避免风控中断导致队尾视频源长期饥饿的问题

--- crates/bili_sync/src/bilibili/credential.rs
+++ crates/bili_sync/src/bilibili/credential.rs
@@ -94,9 +94,8 @@ JNrRuoEUXpabUzGB8QIDAQAB
         .expect("fail to decode public key");
         let ts = chrono::Local::now().timestamp_millis();
         let data = format!("refresh_{}", ts).into_bytes();
-        let mut rng = rand::rng();
         let encrypted = key
-            .encrypt(&mut rng, Oaep::new::<Sha256>(), &data)
+            .encrypt(&mut rand::rng(), Oaep::new::<Sha256>(), &data)
             .expect("fail to encrypt");
         hex::encode(encrypted)
     }
