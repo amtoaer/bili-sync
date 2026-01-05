97e1b6285ecfe2a068c4628a5037c51e1f243b9d
ᴀᴍᴛᴏᴀᴇʀ <amtoaer@gmail.com>
2026-01-05 12:13:50 +0800

    feat: bind_address 绑定失败后尝试 fallback 到默认地址，避免无法启动 web 服务 (#590)

--- crates/bili_sync/src/config/mod.rs
+++ crates/bili_sync/src/config/mod.rs
@@ -8,6 +8,7 @@ mod versioned_config;
 
 pub use crate::config::args::{ARGS, version};
 pub use crate::config::current::{CONFIG_DIR, Config};
+pub(crate) use crate::config::default::default_bind_address;
 pub use crate::config::handlebar::TEMPLATE;
 pub use crate::config::item::{ConcurrentDownloadLimit, NFOTimeType, PathSafeTemplate, RateLimit, Trigger};
 pub use crate::config::versioned_cache::VersionedCache;
