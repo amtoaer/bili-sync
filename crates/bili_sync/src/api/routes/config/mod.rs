c4b227e26e216514a591aeb3816906998820deea
ᴀᴍᴛᴏᴀᴇʀ <amtoaer@gmail.com>
2026-04-08 00:26:46 +0800

    feat: telegram 通知渠道支持仅发送文字 (#701)

--- crates/bili_sync/src/api/routes/config/mod.rs
+++ crates/bili_sync/src/api/routes/config/mod.rs
@@ -9,7 +9,7 @@ use sea_orm::DatabaseConnection;
 use crate::api::wrapper::{ApiError, ApiResponse, ValidatedJson};
 use crate::bilibili::BiliClient;
 use crate::config::{Config, VersionedConfig};
-use crate::notifier::Notifier;
+use crate::notifier::{Message, Notifier};
 
 pub(super) fn router() -> Router {
     Router::new()
@@ -41,7 +41,10 @@ pub async fn ping_notifiers(
         *ignore_cache = Some(());
     }
     notifier
-        .notify(bili_client.inner_client(), "This is a test notification from BiliSync.")
+        .notify(bili_client.inner_client(), Message{
+            message: "This is a test notification from BiliSync.".into(),
+            image_url: Some("https://socialify.git.ci/amtoaer/bili-sync/image?description=1&font=KoHo&issues=1&language=1&logo=https%3A%2F%2Fs2.loli.net%2F2023%2F12%2F02%2F9EwT2yInOu1d3zm.png&name=1&owner=1&pattern=Signal&pulls=1&stargazers=1&theme=Light".to_owned()),
+        })
         .await?;
     Ok(ApiResponse::ok(()))
 }
